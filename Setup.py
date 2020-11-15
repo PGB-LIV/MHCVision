8f3feb863cbd086e1c999820e0f76983ba86d0c8
Phorutai <37576892+Phorutai@users.noreply.github.com>
2020-11-15 01:00:23 +0000

    setup.py file 201115

---
+++ Setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name="noname",
+    version="1.0",
+    description="FDR/PEP estimation for MHC-peptide binding prediction",
+    author='Phorutai Pearngam',
+    author_email='p.pearngam@liverpool.ac.uk',
+    url="https://github.com/Phorutai/FDR_estimation/",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    classifiers="Programming Language :: Python :: 3.7",
+
+)
